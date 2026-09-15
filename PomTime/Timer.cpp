571e81a9aa38f68becc5b6d694cbd9ef02b6b397
RealRyanJames <realcoder2023@gmail.com>
2026-09-15 15:00:15 -0400

    Add project files.

---
+++ PomTime/Timer.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <Windows.h>
+
+namespace SetTitle {
+
+	static std::string SetTitle(std::string titleValue) {
+
+		std::transform(titleValue.begin(), titleValue.end(), titleValue.begin(), ::toupper);
+		return titleValue;
+	}
+
+}
+
+namespace Convert {
+
+	float seconds;
+	float hours;
+
+	static std::map<std::string, std::string> GetUI(std::string lines) {
+
+		std::map<std::string, std::string> linesWritten;
+
+		for (int i = 2; i < 20 / 2; i++) {
+
+			linesWritten.insert({ "", lines });
+		}
+
+		return linesWritten;
+	}
+}
+
+class Title {
+
+public:
+
+	static std::string getTitle(std::string title) {
+
+
+		return title;
+	}
+
+};
+
+class TimerStart {
+
+public:
+
+	static int initialized(int startedIndex) {
+
+		startedIndex = -1;
+		return startedIndex + 1;
+	}
+
+	static void getLoopedUI(int index) {
+
+		if (initialized(0) > -1) {
+
+			Sleep(1 * 600);
+		}
+	}
+};
+
+static std::string AppendMessageEnded(std::string message) {
+
+	return message.append("DONE");
+}
+
+const float LINES_SIZE = 20.0f * 2.0f;
+
+int main() {
+
+	std::string linesGet = "-";
+	std::map<std::string, std::string> lines = Convert::GetUI(linesGet);
+
+	for (auto& line : lines) {
+		for (int i = 0; i < LINES_SIZE; i++) {
+
+			std::cout << line.second;
+		}
+	}
+
+	std::cout << std::endl;
+
+	std::cout << SetTitle::SetTitle(Title::getTitle("Pom Timer")) << std::endl;
+
+	std::cout << std::endl;
+	int i = 1;
+
+	
+	while (i >= 0) {
+
+		i += 1;
+
+		if (i == 25 * 60) {
+
+			std::cout << AppendMessageEnded("Done") << std::endl;
+			Sleep(2 * 1000);
+			break;
+		}
+
+
+		else {
+
+			TimerStart startingTimer;
+			startingTimer.getLoopedUI(i);
+		}
+	}
+
+	for (auto& line : lines) {
+		for (int i = 0; i < LINES_SIZE; i++) {
+			std::cout << line.second;
+		}
+	}
+
+	std::cout << std::endl;
+
+	system("pause > 0");
+	return 0;
+}
